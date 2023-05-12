703c2ca111a1fe0e2767211f7254887d21d8d40f
Andres_Silva <an.silvaa@duocuc.cl>
2023-05-12 10:18:00 -0400

    Correción modal login... Se usó el SessionStorage

--- js/login.js
+++ js/login.js
@@ -15,3 +15,17 @@ function entrar(){
     
 }
 
+window.onload = function() {
+    var modal = document.getElementById("cuadro");
+    
+    // Comprobar si el modal ya se ha mostrado anteriormente
+    var modalShown = sessionStorage.getItem("modalShown");
+    
+    if (!modalShown) {
+        // Mostrar el modal si no se ha mostrado anteriormente
+        modal.style.display = "block";
+        sessionStorage.setItem("modalShown", true);
+    }
+    
+}
+
