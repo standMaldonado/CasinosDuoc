f8d0f8ee2fc1ba283d6bbb4c7e860c7b47ae50e9
StandMaldonado <And.maldonadoh@duocuc.cl>
2023-05-13 17:49:49 -0400

    sigo cometiendolo

--- js/formulario.js
+++ js/formulario.js
@@ -1,38 +1,48 @@
 $.validator.addMethod("correito", function (value, element) {
     var pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
     return this.optional(element) || pattern.test(value);
- }, "Formato del email incorrecto");
+ }, "Formato del correo incorrecto");
+
+ $.validator.addMethod("nombresito", function (value, element) {
+    var pattern = /^[a-zA-Z]+$/;
+    return this.optional(element) || pattern.test(value);
+ }, "Formato del nombre incorrecto");
+
 
 $("#formulario").validate({
     rules: {
         nombre: {
             required: true,
-            minlength: 5,
-            maxlength: 40
+            minlength: 3,
+            maxlength: 40,
+            nombresito: true
         },
         email: {
             required: true,
             email: true,
             correito: true
         },
-        rut: {
+        
+        subject:{
             required: true,
-            minlength: 9,
-            number : true
+            minlength: 10,
+            maxlength: 30
         },
+
         comentario:{
             required: true,
-            minlength: 50
+            minlength: 30
         }
     }
 })
 
 $("#enviar").click(function(){
-    if($("formulario").valid() == false){
+    if($("#formulario").valid() == false){
         return; 
     } 
     let nombre = $("nombre").val()
     let email = $("email").val()
+    let asunto = $("asunto").val()
     let comentario = $("comentario").val()
     
 })
