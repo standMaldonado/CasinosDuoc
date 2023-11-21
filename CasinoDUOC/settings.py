9e61c0936be7cec5262f4e01fa5990c9c50a99da
StandMaldonado <and.maldonadoh@duocuc.cl>
2023-11-21 20:54:17 -0300

    inicio feo

--- CasinoDUOC/settings.py
+++ CasinoDUOC/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mecanico',
         'USER':'root',
-        'PASSWORD':'1234567',
+        'PASSWORD':'1234',
         'HOST':'localhost',
         'PORT':'3306'
     }
