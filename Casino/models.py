354164c3a7b8b6fb08d8ac446f540ea6ef7e5af3
Wachitugo <107078547+Wachitugo@users.noreply.github.com>
2023-11-21 00:02:44 -0300

    ooo

--- Casino/models.py
+++ Casino/models.py
@@ -1,19 +1,4 @@
-from django.db import models
 
-class COMIDA(models.Model):
-    id_comida = models.AutoField(db_column='IdComida', primary_key=True)
-    nombre = models.CharField(max_length=30,blank=False,null=False)
-    descripcion = models.CharField(max_length=200,blank=False,null=False)
-    def __str__(self):
-        return str(self.nombre)
-
-class BEBIDA(models.Model):
-    id_bebida = models.AutoField(db_column='IdBebida', primary_key=True)
-    nombre = models.CharField(max_length=30,blank=False,null=False)
-    descripcion = models.CharField(max_length=200,blank=False,null=False)
-    def __str__(self):
-        return str(self.nombre)
-    
     
     
 
--- CasinoDUOC/settings.py
+++ CasinoDUOC/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'CasinoDUOC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mecanico',
+        'USER':'root',
+        'PASSWORD':'1234567',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
