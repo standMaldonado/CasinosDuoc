50ba6828bca43b67c48752e69b06041b9533b02e
Wachitugo <107078547+Wachitugo@users.noreply.github.com>
2023-11-22 15:59:20 -0300

    imagenes

--- Casino/urls.py
+++ Casino/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'Casino'
 urlpatterns= [
     path('', views.index, name='index'),
-    path("menu/", views.menu, name='menu'),
-    path('mi_perfil/', views.mi_perfil, name='mi_perfil'),
+    path("productos/", views.menu, name='menu'),
+    path('Servicios/', views.mi_perfil, name='mi_perfil'),
     path("somos/", views.somos, name='somos'),
     path("soporte/", views.soporte, name='soporte'),
     
